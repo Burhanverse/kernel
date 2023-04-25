b8c2f2733ad4d1459b5ece9a5806bfabe6bbbca5
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-04-25 14:12:33 +0000

    MT6768 : Import Script for DEFCONFIG Regen

---
+++ regen_def.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DEFCONFIG="lava_defconfig"
+
+make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG"
+cp -af out/.config arch/arm64/configs/"$DEFCONFIG"
+git add arch/arm64/configs/"${DEFCONFIG}"
+git commit -m "LAVA:defconfig:Regenerate"
+echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
