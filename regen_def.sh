e4ef3fe0e5469832e0fe5ab00d12827e1d57d10e
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-07-09 12:18:33 +0000

    regen-script : Add Merlin Support

--- regen_def.sh
+++ regen_def.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 DEFCONFIG="lava_defconfig"
+DEFCONFIG_1="merlin_defconfig"
 
 make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG"
 cp -af out/.config arch/arm64/configs/"$DEFCONFIG"
-git add arch/arm64/configs/"${DEFCONFIG}"
-git commit -m "LAVA:defconfig:Regenerate"
+make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG_1"
+cp -af out/.config arch/arm64/configs/"$DEFCONFIG_1"
+git add arch/arm64/configs
+git commit -m "XM6768:defconfig:Regenerate"
 echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
