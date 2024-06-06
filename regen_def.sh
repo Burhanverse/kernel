c7fef51a4adc6302363f7259d48d36a4bafdf747
Aqua <s24.burhan@gmail.com>
2024-06-06 15:00:19 +0000

    MT6768 : Rebrand to Raider

--- regen_def.sh
+++ regen_def.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEFCONFIG="lava_defconfig"
+DEFCONFIG="lancelot_defconfig"
 DEFCONFIG_1="merlin_defconfig"
 
 make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG"
@@ -8,5 +8,5 @@ cp -af out/.config arch/arm64/configs/"$DEFCONFIG"
 make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG_1"
 cp -af out/.config arch/arm64/configs/"$DEFCONFIG_1"
 git add arch/arm64/configs
-git commit -m "XM6768:defconfig:Regenerate"
+git commit -m "MT6768:defconfig:Regenerate"
 echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
